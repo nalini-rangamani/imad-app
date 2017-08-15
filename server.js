10977ec9daadbd27d77f0fef737007b500f5106c
nalini-rangamani <30622688+nalini-rangamani@users.noreply.github.com>
2017-08-15 10:52:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,11 +22,11 @@ app.get('/article1', function (req, res) {
 });
 
 app.get('/article2', function (req, res) {
-  res.send("article2 by Nalini");
+  res.sendFile(path.join(__dirname, 'ui', 'article-two2.html'));
 });
 
 app.get('/article3', function (req, res) {
-  res.send("article3 by Nalini");
+  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 
