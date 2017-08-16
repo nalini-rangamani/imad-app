6b5505e823f95d592ef945647a6a71cefbe93b4c
nalini-rangamani <30622688+nalini-rangamani@users.noreply.github.com>
2017-08-16 13:12:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,13 @@ console.log('Loaded!');
 var Elem1=document.getElementById('main-text2');
 Elem1.innerHTML='my own text replaced programmatically';
 var Image1=document.getElementById('main-image1');
+
+var marginLeft=0;
+function MoveToRight(){
+marginLeft+= 1;
+Image1.style.marginLeft =  marginLeft + 'px';
+};
+
 Image1.onclick = function() {
-    Image1.style.marginLeft = '100px';
+  setInterval(MoveToRight,50);  
 };
