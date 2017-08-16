ae20fb78bd0683113097ad8dabf2b0a2e46f3655
nalini-rangamani <30622688+nalini-rangamani@users.noreply.github.com>
2017-08-16 15:00:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,12 @@
-
+//counter of views
+var counter=0;
+var js_counter = document.getElementById('counter');
+js_counter.onclick= function() {
+  counter++;
+  var js_count=document.getElementById('count');
+  js_count.innerHTML=counter.toString();
+};
+/* 
 console.log('Loaded!');
 //to change a html element
 var Elem1=document.getElementById('main-text2');
@@ -16,3 +24,4 @@ Image1.style.marginLeft =  marginLeft + 'px';
 Image1.onclick = function() {
   setInterval(MoveToRight,50);  
 };
+*/
