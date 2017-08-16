7de3844fe58b14e577ea57c939a12f6e1daa4a63
nalini-rangamani <30622688+nalini-rangamani@users.noreply.github.com>
2017-08-16 13:01:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,8 @@
 console.log('Loaded!');
 //to change a html element
-var Elem1=document.getElementById('main-text1');
+var Elem1=document.getElementById('main-text2');
 Elem1.innerHTML='my own text replaced programmatically';
-
+var Image1=document.getElementById('main-image1');
+Image1.onClick = function() {
+    Image1.style.marginLeft = '100px';
+};
