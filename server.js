da117f50a166050b757923a20831d36716dca60a
nalini-rangamani <30622688+nalini-rangamani@users.noreply.github.com>
2017-08-20 15:17:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(morgan('combined'));
 
 var names=[];
 app.get('/submit-name',function(req,res){
-    var name = req.params.name;
+    var name = req.query.name;
     names.push(name);
     res.send(JSON.stringify(names));
 });
