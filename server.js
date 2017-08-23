e3febfe8e0b43f39f12f67e474ac655e87c5fb0b
nalini-rangamani <30622688+nalini-rangamani@users.noreply.github.com>
2017-08-23 18:09:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
-var path = require('path');
+var path = require('path')
+var Pool = require('pg').pool;
+
+var config={
+    user : 'nalinirangamani',
+    database : 'nalinirangamani',
+    host : 'db.imad.hasura-app.io',
+    port : '5432',
+    password : process.env.DB_PASSWORD
+}
 
 var app = express();
 app.use(morgan('combined'));
@@ -11,7 +20,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var pool = new Pool(config);
 app.get*'/test-db',function(req,res){
+    pool.query('select * from test',function(err,result){
+    if (err) {
+    send.status(500).send(err.toString());
+    }
+    else
+    {
+        res.send(JSON.stringify(result));
+    }
+    }
+    )
 };
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
