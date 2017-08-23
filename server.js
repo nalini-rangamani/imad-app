ae1bb3915dcb2f016b2315f741ff2810a71a9a9c
nalini-rangamani <30622688+nalini-rangamani@users.noreply.github.com>
2017-08-23 18:10:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/', function (req, res) {
 
 var pool = new Pool(config);
 app.get*'/test-db',function(req,res){
-    pool.query('select * from test',function(err,result){
+    pool.query('SELECT * from test',function(err,result){
     if (err) {
     send.status(500).send(err.toString());
     }
